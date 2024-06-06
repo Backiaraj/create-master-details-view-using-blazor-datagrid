a79d3273228d829df68ebee0b2ad5366c8049a2b
Backiaraj <backiaraj.kanagaraj@syncfusion.com>
2024-06-06 17:20:53 +0530

    update the project

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -1,19 +1,24 @@
-namespace MasterDetailViewGridSample.Data
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Master_Detail_Grid.Data
 {
     public class WeatherForecastService
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
+            var rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToArray());
         }
     }
